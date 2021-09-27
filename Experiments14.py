dad23838be128f1fa1eabb35048504853eeddc4f
Nax Arakawa <thatonecoder420@gmail.com>
2021-09-27 17:29:10 +0800

    Week 7 Class Completed
    Learned about lists.

---
+++ Experiments14.py
@@ -0,0 +1,7 @@
+# lists
+
+empty = []
+team = ['Checlsea', 'Stamford Bridge', 1905]
+num = [5, 10, 4, 5]
+words = ['spam', 'eggs']
+
---
+++ Experiments15.py
@@ -0,0 +1,6 @@
+A = float(input('Enter first number: '))
+B = float(input('Enter second number: '))
+C = float(input('Enter third number: '))
+
+result = (A + B + C) / 3
+print(float(result))
